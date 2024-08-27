fe371b00e962c040c31d1372c54aaeabe6ec7696
jataylor111 <143449478+jataylor111@users.noreply.github.com>
2024-08-27 14:17:06 -0600

    Update code.js

--- code.js
+++ code.js
@@ -1,5 +1,5 @@
 function sum(a) {
-    var sum = a[0];
+    var sum = 0; //start at 0 instead of a[0] to take empty lists into account
     for(var i = 0; i < a.length; i++) {
         sum += a[i];
     }
